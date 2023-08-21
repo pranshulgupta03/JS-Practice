78858ca9de5e45aafaae7b1310e6136df2a7af37
pranshulgupta03 <guptapranshu0801@gmail.com>
2023-08-21 10:54:52 +0530

    day1 file modified

--- day1.js
+++ day1.js
@@ -8,14 +8,12 @@
 }
 example()
 console.log(x)
-console.log(y) */ 
-
+console.log(y) */
 
 // Hoisting
 // console.log(a)
 // const a = 10
 // console.log(a)
-
 
 /* function calculator(a,b,callback) {
     return callback(a, b);
@@ -29,8 +27,7 @@ function mul(a, b) {
 var res = calculator(5, 3, mul);
 console.log(res)
 var res = calculator(5, 3, add);
-console.log(res) */ 
-
+console.log(res) */
 
 // CALLBACK HELL
 /* const placeorder = (callback) => {
@@ -79,66 +76,64 @@ placeorder(() => {
     })
 }) */
 
-
-
-const placeorder = () => {
+const placeorder = (callback) => {
     setTimeout(() => {
-        console.log('Order placed');
-        bookticket();
-    }, 2000)
-}
-const bookticket = () => {
+        console.log("Order placed");
+        callback();
+    }, 2000);
+};
+const bookticket = (callback) => {
     setTimeout(() => {
-        console.log('Ticket booked');
-        boardingpass();
+        console.log("Ticket booked");
+        callback();
     }, 1000);
-}
-const boardingpass = () => {
+};
+const boardingpass = (callback) => {
     setTimeout(() => {
-        console.log('Boarding pass issued');
-        securitycheck();
-    }, 2000)
-}
-const securitycheck = () => {
+        console.log("Boarding pass issued");
+        callback();
+    }, 2000);
+};
+const securitycheck = (callback) => {
     setTimeout(() => {
-        console.log("security checked pass")
-        selfcheck();
-    }, 2000)
-}
-const selfcheck = () => {
+        console.log("security checked pass");
+        callback();
+    }, 2000);
+};
+const selfcheck = (callback) => {
     setTimeout(() => {
-        console.log("self checked pass")
-        navigationpanel();
-    }, 2000)
-}
-const navigationpanel = () => {
+        console.log("self checked pass");
+        callback();
+    }, 2000);
+};
+const navigationpanel = (callback) => {
     setTimeout(() => {
-        console.log("navigation panel crossed")
-        buspickup();
-    }, 2000)
-}
-const buspickup = () => {
+        console.log("navigation panel crossed");
+        callback();
+    }, 2000);
+};
+const buspickup = (callback) => {
     setTimeout(() => {
-        console.log("pickup bus reached")
-        airplanetakeoff();
-    }, 2000)
-}
-const airplanetakeoff = () => {
+        console.log("pickup bus reached");
+        callback();
+    }, 2000);
+};
+const airplanetakeoff = (callback) => {
     setTimeout(() => {
-        console.log("airplane takeoff")
-        aircraft();
-    }, 2000)
-}
-const aircraft = () => {
+        console.log("airplane takeoff");
+        callback();
+    }, 2000);
+};
+const aircraft = (callback) => {
     setTimeout(() => {
         console.log("aircraft");
-        destination();
-    }, 2000)
-}
+        callback();
+    }, 2000);
+};
 const destination = () => {
-        console.log("detination reached")
-}
-console.log('booking ticket');
+    console.log("detination reached");
+};
+console.log("booking ticket");
 placeorder(() => {
     bookticket(() => {
         boardingpass(() => {
@@ -148,14 +143,14 @@ placeorder(() => {
                         buspickup(() => {
                             airplanetakeoff(() => {
                                 aircraft(() => {
-                                    destination()
-                                    console.log('journey completed');    
-                                })
-                            })
-                        })
-                    })
-                })
-            })
-        })
-    })
-})
+                                    destination();
+                                    console.log("journey completed");
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
